ec9ab230c5896f51e303a2b50fe2e1ebc2fe6a3d
veer2238 <114563813+veer2238@users.noreply.github.com>
2023-04-08 16:05:31 +0530

    fvb

---
+++ Line.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Line = () => {
+  return (
+    <div>
+
+    <hr/>
+
+    <hr
+    style={{
+      background: 'lime',
+      color: 'lime',
+      borderColor: 'lime',
+      height: '10px',
+    }}
+  />
+
+ 
+
+
+
+
+  <div style={{flex: 1, height: '1px', backgroundColor: 'black'}} />
+ 
+
+    </div>
+  )
+}
+
+export default Line
